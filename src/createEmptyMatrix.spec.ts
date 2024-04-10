a591f82ef071fa7b1191e2f4a57cdaa9b2ff7a79
James Beasley-Gard <james.beasleygard@journeyone.com.au>
2024-04-10 12:09:07 +0800

    GREEN: createEmptyMatrix returns a m*n matrix given values for m and n

--- src/createEmptyMatrix.spec.ts
+++ src/createEmptyMatrix.spec.ts
@@ -5,7 +5,13 @@ describe('createEmptyMatrix', () => {
   it('returns a 0x0 matrix given defaults', () => {
     expect(createEmptyMatrix()).toEqual([])
   })
-  it('returns a 0xColumns matrix given columns', () => {
-    expect(createEmptyMatrix(7)).toEqual([[], [], [], [], [], [], []])
+  it('returns a m*n matrix given values for m and n', () => {
+    const matrix = createEmptyMatrix(3, 4)
+    expect(matrix).toHaveLength(3)
+    expect(matrix).toEqual([
+      [undefined, undefined, undefined, undefined],
+      [undefined, undefined, undefined, undefined],
+      [undefined, undefined, undefined, undefined],
+    ])
   })
 })
--- src/createEmptyMatrix.ts
+++ src/createEmptyMatrix.ts
@@ -1,9 +1,9 @@
-const createEmptyMatrix = (columns?: number): Array<Array<1 | 2 | undefined>> => {
-  if (columns === undefined) {
+const createEmptyMatrix = (m?: number, n?: number): Array<Array<1 | 2 | undefined>> => {
+  if (n === undefined) {
     return []
   }
 
-  return [...Array(columns)].map(() => [])
+  return [...Array(m)].map((): Array<1 | 2 | undefined> => Array(n).fill(undefined))
 }
 
 export default createEmptyMatrix

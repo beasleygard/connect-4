1b4470a068fc19f36621d9a12489af7513c18730
James Beasley-Gard <james.beasleygard@journeyone.com.au>
2024-05-14 16:24:16 +0800

    RED/GREEN: should fail when given objects are the same object

--- src/connect4-domain/to-be-deeply-unequal.ts
+++ src/connect4-domain/to-be-deeply-unequal.ts
@@ -1,3 +1,15 @@
-const fn = (x: any) => x
+import { MatcherResult } from '@/vitest'
 
-export default fn
+function toBeDeeplyUnequal(
+  this: { isNot: boolean } | void,
+  received: object,
+  expected: object,
+): MatcherResult {
+  const isNot = this?.isNot ?? false
+  return {
+    pass: received !== expected,
+    message: () => `Objects are deeply ${isNot ? 'un' : ''}equal`,
+  }
+}
+
+export default toBeDeeplyUnequal

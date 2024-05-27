e50c63d004ebd60fd4869e58eefbb7bbcfdd4575
James Beasley-Gard <james.beasleygard@journeyone.com.au>
2024-05-27 16:59:24 +0800

    GREEN: should return a deep copy of an array

--- src/connect4-domain/deep-clone.ts
+++ src/connect4-domain/deep-clone.ts
@@ -1,5 +1,22 @@
 function deepClone<T>(value: T): T {
-  return value
+  if (!(value instanceof Object)) {
+    return value
+  }
+
+  let result: T
+
+  if (Array.isArray(value)) {
+    result = [] as T
+  } else {
+    result = {} as T
+  }
+
+  for (const key of Object.keys(value)) {
+    const nestedValue = (value as any)[key]
+    ;(result as any)[key] = deepClone<typeof nestedValue>(nestedValue)
+  }
+
+  return result
 }
 
 export default deepClone

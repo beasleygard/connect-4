e78093a53b5267d16711068930ec9b19cb8c6eb3
James Beasley-Gard <james.beasleygard@journeyone.com.au>
2024-05-27 14:45:25 +0800

    GREEN: should fail when one object has an additional key and shares a reference to a value

--- src/connect4-domain/to-be-deeply-unequal.spec.ts
+++ src/connect4-domain/to-be-deeply-unequal.spec.ts
@@ -11,6 +11,17 @@ describe('toBeDeeplyUnequal', () => {
     const obj2 = {}
     expect(obj1).toBeDeeplyUnequal(obj2)
   })
+  it('should pass when one object has an additional key at a shallow level', () => {
+    const obj1 = { a: 1, b: 2 }
+    const obj2 = { a: 1, b: 2, c: 3 }
+    expect(obj1).toBeDeeplyUnequal(obj2)
+  })
+  it('should fail when one object has an additional key and shares a reference to a value', () => {
+    const innerArray = []
+    const obj1 = { a: 1, b: innerArray }
+    const obj2 = { a: 1, b: innerArray }
+    expect(obj1).not.toBeDeeplyUnequal(obj2)
+  })
   it('should pass given an array and an object', () => {
     const arr = []
     const obj = {}
@@ -35,8 +46,8 @@ describe('toBeDeeplyUnequal', () => {
     expect(arr1).toBeDeeplyUnequal(arr2)
   })
   it('should pass given two arrays that are unequal at a shallow level', () => {
-    const arr1 = [1]
-    const arr2 = [2]
+    const arr1 = [1, 2]
+    const arr2 = [1, 3]
     expect(arr1).toBeDeeplyUnequal(arr2)
   })
   it('should fail given two arrays that are equal at a shallow level', () => {
--- src/connect4-domain/to-be-deeply-unequal.ts
+++ src/connect4-domain/to-be-deeply-unequal.ts
@@ -1,13 +1,34 @@
 import { MatcherResult } from '@/vitest'
 
+function getCannotFindSharedReference(obj1: any, obj2: any): boolean {
+  const firstIsObject = obj1 instanceof Object
+  const secondIsObject = obj2 instanceof Object
+  if (!(firstIsObject && secondIsObject)) {
+    return true
+  }
+  if (obj1 === obj2) {
+    return false
+  }
+
+  const keys = new Set([...Object.keys(obj1), ...Object.keys(obj2)])
+  for (const key of keys) {
+    if (!getCannotFindSharedReference(obj1[key], obj2[key])) {
+      return false
+    }
+  }
+
+  return true
+}
+
 function toBeDeeplyUnequal(
   this: { isNot: boolean } | void,
   received: object,
   expected: object,
 ): MatcherResult {
   const isNot = this?.isNot ?? false
+  let objsAreDeeplyUnequal = getCannotFindSharedReference(received, expected)
   return {
-    pass: received !== expected,
+    pass: objsAreDeeplyUnequal,
     message: () => `Objects are deeply ${isNot ? 'un' : ''}equal`,
   }
 }

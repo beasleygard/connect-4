8c94a05ac7a0ba34a03d7a62360f01e591fcb292
James Beasley-Gard <james.beasleygard@journeyone.com.au>
2024-05-28 16:23:52 +0800

    RED: should return a createMovePlayerCommand

---
+++ src/connect4-domain/commands.spec.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from 'vitest'
+
+describe('commands', () => {
+  describe('createMovePlayerCommand', () => {
+    it('should return a createMovePlayerCommand', () => {
+      const movePlayerCommand = createMovePlayerCommand({
+        player: 1,
+        targetCell: { row: 0, column: 0 },
+      })
+      expect(movePlayerCommand).toBeInstanceOf(MovePlayerCommand)
+      expect(movePlayerCommand).toEqual({
+        type: 'MOVE_PLAYER',
+        payload: {
+          player: 1,
+          targetCell: { row: 0, column: 0 },
+        },
+      })
+    })
+  })
+})

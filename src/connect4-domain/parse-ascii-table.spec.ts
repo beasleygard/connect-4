cfb1b6f8b6ea0e132d1a93dd605d36ee421e3ae2
James Beasley-Gard <james.beasleygard@journeyone.com.au>
2024-06-04 14:35:47 +0800

    RED: parse-ascii-table > given a table with no rows or columns returns an empty grid

---
+++ src/connect4-domain/parse-ascii-table.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest'
+import parseAsciiTable from '@/connect4-domain/parse-ascii-table'
+
+describe('parse-ascii-table', () => {
+  describe('given a table with no rows or columns', () => {
+    it('returns an empty grid', () => {
+      const emptyTable = ''
+      expect(parseAsciiTable(emptyTable)).toEqual([[]])
+    })
+  })
+})

2ad886de6806d78951bdf7e4a8424d852594cbf0
James Beasley-Gard <james.beasleygard@journeyone.com.au>
2024-06-04 15:33:30 +0800

    RED: parse-ascii-table > given a 1x1 ascii table with a non-empty cell and a custom cell resolver returns a 1x1 grid with a resolved value

--- src/connect4-domain/parse-ascii-table.spec.ts
+++ src/connect4-domain/parse-ascii-table.spec.ts
@@ -1,6 +1,8 @@
 import { describe, expect, it } from 'vitest'
 import parseAsciiTable from '@/connect4-domain/parse-ascii-table'
 
+const stringToNumberResolver = (input: string): number => Number(input)
+
 describe('parse-ascii-table', () => {
   describe('given a table with no rows or columns', () => {
     it('returns an empty grid', () => {
@@ -24,6 +26,15 @@ describe('parse-ascii-table', () => {
 | 1 |
 |---|`
         expect(parseAsciiTable(asciiTable)).toEqual([['1']])
+      })
+      describe('and a custom cell resolver', () => {
+        it('returns a 1x1 grid with a resolved value', () => {
+          const asciiTable = `
+        |---|
+        | 1 |
+        |---|`
+          expect(parseAsciiTable(asciiTable, stringToNumberResolver)).toEqual([[1]])
+        })
       })
     })
   })
--- src/connect4-domain/parse-ascii-table.ts
+++ src/connect4-domain/parse-ascii-table.ts
@@ -1,22 +1,21 @@
+type NullableString = string | null
+
 function parseAsciiTable<T>(asciiTable: string): Array<Array<T>> {
-  const tableContent: Array<Array<string>> = asciiTable
+  return asciiTable
     .split('\n')
     .filter((_, index, rows) => index % 2 === 0 && index != 0 && index < rows.length - 1)
-    .map((row) => row.match(/(?<=\| )(.*?)(?= \|)/gm)) as Array<Array<string>>
-  return tableContent.reduce(
-    (parsedRows: Array<Array<T>>, rowContent: Array<string>): Array<Array<T>> => {
+    .map((row) => row.match(/(?<=\| )(.*?)(?= \|)/gm) as Array<NullableString>)
+    .reduce((parsedRows: Array<Array<T>>, rowContent: Array<NullableString>): Array<Array<T>> => {
       parsedRows.push(
-        rowContent.reduce((parsedCells: Array<T>, cellContent: string): Array<T> => {
-          if (cellContent.length > 0) {
+        rowContent.reduce((parsedCells: Array<T>, cellContent: NullableString): Array<T> => {
+          if (cellContent !== null && cellContent.length > 0) {
             parsedCells.push(cellContent as T)
           }
           return parsedCells
         }, []),
       )
       return parsedRows
-    },
-    [],
-  )
+    }, [])
 }
 
 export default parseAsciiTable

8996cd0285e331a126c1d31b1c0707e723120ccb
James Beasley-Gard <james.beasleygard@journeyone.com.au>
2024-06-04 14:48:43 +0800

    RED: parse-ascii-table > given a 1x1 ascii table returns a 1x1 grid

--- src/connect4-domain/parse-ascii-table.spec.ts
+++ src/connect4-domain/parse-ascii-table.spec.ts
@@ -1,11 +1,23 @@
 import { describe, expect, it } from 'vitest'
 import parseAsciiTable from '@/connect4-domain/parse-ascii-table'
+import toAsciiTable from '@/connect4-domain/to-ascii-table'
 
 describe('parse-ascii-table', () => {
   describe('given a table with no rows or columns', () => {
     it('returns an empty grid', () => {
       const emptyTable = ''
       expect(parseAsciiTable(emptyTable)).toEqual([])
+    })
+  })
+  describe('given a 1x1 ascii table', () => {
+    it('returns a 1x1 grid', () => {
+      const asciiTable = `
+        "
+        |---|
+        |   |
+        |---|"
+      `
+      expect(parseAsciiTable(asciiTable)).toEqual([[]])
     })
   })
 })

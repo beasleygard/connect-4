c353b61c45e03a3a6475a0d344ebb81753a85d2a
James Beasley-Gard <james.beasleygard@journeyone.com.au>
2024-06-04 15:20:14 +0800

    RED: parse-ascii-table > given a 1x1 ascii table with a non-empty cell returns a 1x1 grid

--- src/connect4-domain/parse-ascii-table.spec.ts
+++ src/connect4-domain/parse-ascii-table.spec.ts
@@ -1,6 +1,5 @@
 import { describe, expect, it } from 'vitest'
 import parseAsciiTable from '@/connect4-domain/parse-ascii-table'
-import toAsciiTable from '@/connect4-domain/to-ascii-table'
 
 describe('parse-ascii-table', () => {
   describe('given a table with no rows or columns', () => {
@@ -10,13 +9,22 @@ describe('parse-ascii-table', () => {
     })
   })
   describe('given a 1x1 ascii table', () => {
-it('returns a 1x1 grid', () => {
+    it('returns a 1x1 grid', () => {
       const asciiTable = `
 |--|
 |  |
 |--|
 `
       expect(parseAsciiTable<undefined>(asciiTable)).toEqual([[]])
+    })
+    describe('with a non-empty cell', () => {
+      it('returns a 1x1 grid', () => {
+        const asciiTable = `
+|---|
+| 1 |
+|---|`
+        expect(parseAsciiTable(asciiTable)).toEqual([['1']])
+      })
     })
   })
 })

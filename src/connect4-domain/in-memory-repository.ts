9b991f689ac03831f96f0d21c43287c78459f6cf
James Beasley-Gard <james.beasleygard@journeyone.com.au>
2024-06-17 16:44:30 +0800

    GREEN: in-memory-repository > saves a board

--- src/connect4-domain/in-memory-repository.ts
+++ src/connect4-domain/in-memory-repository.ts
@@ -1,5 +1,25 @@
-interface GameRepository {}
+import { Board } from '@/connect4-domain/game'
 
-class InMemoryRepository implements GameRepository {}
+type BoardId = `${string}-${string}-${string}-${string}-${string}`
+
+type BoardStore = Map<BoardId, Board>
+
+interface GameRepository {
+  save: (board: Board) => BoardId
+}
+
+class InMemoryRepository implements GameRepository {
+  private store: BoardStore
+
+  constructor(store: BoardStore = new Map() as BoardStore) {
+    this.store = store
+  }
+
+  save(board: Board): BoardId {
+    const boardId = crypto.randomUUID()
+    this.store.set(boardId, board)
+    return boardId
+  }
+}
 
 export default InMemoryRepository

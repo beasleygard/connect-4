96709c23cf3ac66664bdfadd360031f1f9f515eb
James Beasley-Gard <james.beasleygard@journeyone.com.au>
2024-07-11 23:04:22 +0800

    Restyle 'start game' button

--- src/connect4-ui/StartGameButton.tsx
+++ src/connect4-ui/StartGameButton.tsx
@@ -7,15 +7,27 @@ type GameStartButtonProps = {
 
 const StyledButton = styled.button`
   padding: 20px 15px;
-  font-family: monospace;
-  font-size: 2rem;
-  background-color: lightblue;
+  font-size: 4rem;
+  background-color: #eceff4;
+  border-radius: 4rem;
+  font-weight: 700;
+  font-style: italic;
   color: black;
   position: fixed;
   margin: auto;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  transition: border-color 0.25s;
+  box-sizing: border-box;
+  border: 1rem solid transparent;
+  &:focus:enabled {
+    outline: 4px auto -webkit-focus-ring-color;
+    outline: none;
+  }
+  &:hover:enabled {
+    border-color: #808386;
+  }
 `
 
 const StartGameButton = ({ onStartGameClick = () => undefined }: GameStartButtonProps) => (
